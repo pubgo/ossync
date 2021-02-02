53060115b4a30b9410375fbbb3ba0f3459493c7d
白云辉 <kooksee@163.com>
2021-02-02 20:42:54 +0800

    fix: update

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"github.com/google/gops/agent"
 	"github.com/pubgo/golug"
 	"github.com/pubgo/ossync/entry"
+	"github.com/pubgo/xerror"
+	"net"
+	"net/http"
+	_ "net/http/pprof"
 )
 
 func main() {
+	//xerror.ExitErr(profiler.Start(profiler.Config{
+	//	ApplicationName: "ossync",
+	//	ServerAddress:   "http://localhost:4040", // this will run inside docker-compose, hence `pyroscope` for hostname
+	//}))
+
+	lis, err := net.Listen("tcp", ":8088")
+	xerror.Exit(err)
+	go http.Serve(lis, nil)
+	xerror.Exit(agent.Listen(agent.Options{}))
 	golug.Run(entry.GetEntry())
 }
